e60ea971ddf94faac920afad23ac40bfdda33a46
Yaniv Solnik <solnik@gmail.com>
2018-06-13 19:44:56 +0300

    changed productIds

--- src/data/offers.js
+++ src/data/offers.js
@@ -3,20 +3,20 @@ module.exports = [
     offerId: 'P102035786_JM',
     secretKey:
       's2eE8iGjt7EGR2b9UOPTyoCbL7W8WS39t3szQEoU2gRqOMkppQA_L5HZG8oE9k21jQuCiCrLlo6t8IT9',
-    appleProductId: 'com.sportsmax.P102035786_JMN',
-    androidProductId: 'com.sportsmax.P102035786_JMN'
+    appleProductId: 'com.sportsmax.p102035786_jmn',
+    androidProductId: 'com.sportsmax.p102035786_jmn'
   },
   {
     offerId: 'P275826709_JM',
     secretKey:
       's2eE8iGjt7EGR2b9UOPTyoCbL7W8WS39t3szQEoU2gRqOMkppQA_L5HZG8oE9k21jQuCiCrLlo6t8IT9',
-    appleProductId: 'com.sportsmax.P275826709_JM',
-    androidProductId: 'com.sportsmax.P275826709_JM'
+    appleProductId: 'com.sportsmax.p275826709_jm',
+    androidProductId: 'com.sportsmax.p275826709_jm'
   },
   {
     offerId: 'S876818327_JM',
     secretKey: '2gRqOMkppQA_L5HZG8oE9k21jQuCiCrLlo6t8IT9',
-    appleProductId: 'com.sportsmax.S876818327_JM',
-    androidProductId: 'com.sportsmax.S876818327_JM'
+    appleProductId: 'com.sportsmax.s876818327_jm',
+    androidProductId: 'com.sportsmax.s876818327_jm'
   }
 ];
