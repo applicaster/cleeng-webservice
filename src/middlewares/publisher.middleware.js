2d3c55515f0c282335d79bf931cc8d2e41f2c35b
Yaniv Solnik <solnik@gmail.com>
2018-07-04 16:17:32 +0300

    added publishers db

--- src/middlewares/publisher.middleware.js
+++ src/middlewares/publisher.middleware.js
@@ -4,8 +4,12 @@ module.exports = async (req, res, next) => {
   try {
     const { publisherId } = req.body;
     req.publisher = await getPublisher(publisherId);
+    if (!req.publisher) {
+      return res.status(401).send('bad publisher');
+    }
   } catch (err) {
     console.log(err);
+    return res.status(401).send('bad publisher');
   }
 
   next();
