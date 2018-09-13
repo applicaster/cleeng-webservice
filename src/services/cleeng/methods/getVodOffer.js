1eb78471361128b2e39d6ca10d15fa3ba3c7dd65
Yaniv Solnik <solnik@gmail.com>
2018-09-13 19:14:47 +0300

    added support for getVOD response from cleeng

--- src/services/cleeng/methods/getVodOffer.js
+++ src/services/cleeng/methods/getVodOffer.js
@@ -1,7 +1,10 @@
 const { post } = require('./post');
 
 const getVodOffer = async data => {
-  return post('getVodOffer', data);
+  return post('getVodOffer', data).then(response => {
+    const { vod } = response;
+    return vod;
+  });
 };
 
 module.exports = { getVodOffer };
