973272d9cd419e43f9ae28352dbeaf463b2b861d
Yaniv Solnik <solnik@gmail.com>
2018-09-28 18:40:16 +0300

    staging related fixes

--- src/services/cleeng/methods/post.js
+++ src/services/cleeng/methods/post.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const config = require('../config');
 
 const post = async (path, params) => {
-  const url = process.CLEENG_BASE_URL || `${config.baseUrl}`;
+  const url = process.env.CLEENG_BASE_URL || `${config.baseUrl}`;
   const method = 'post';
   const id = 1;
   const data = { method: path, params, id, 'json-rpc': '2.0' };
