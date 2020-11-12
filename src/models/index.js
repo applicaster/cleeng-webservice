af73639c70503e2d6b20be00d7a25241da259769
Neer Friedman <neerfri@gmail.com>
2020-11-12 21:58:11 +0200

    add required connection config for mongo atlas

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
+
+
 let dbURI = 'mongodb://localhost/cleeng-publishers';
+mongooseOpts = {
+  useNewUrlParser: true,
+
+}
 if (process.env.MONGODB_URI) {
   dbURI = process.env.MONGODB_URI;
+
+  let dbURLParams = querystring.parse(uri.parse(dbURI).query)
+  mongooseOpts["replicaSet"] = dbURLParams["replicaSet"]
+  mongooseOpts["authSource"] = dbURLParams["authSource"]
 }
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(dbURI, mongooseOpts);
 
 mongoose.connection.on('connected', () => {
   console.log(`Mongoose Conencted:${dbURI}`);
